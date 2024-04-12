5f9b4757bb4c324db1b8be0ae788fe3312331dbf
reinheimermat <mreinheimer0@gmail.com>
2024-04-11 21:52:44 -0300

    style: add colors guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueDark: '#1E6F9F',
+        blue: '#4EA8DE',
+        purple: '#8284FA',
+        purpleDark: '#5E60CE',
+        grey: {
+          100: '#F2F2F2',
+          200: '#D9D9D9',
+          300: '#808080',
+          400: '#333333',
+          500: '#262626',
+          600: '#1A1A1A',
+          700: '#0D0D0D',
+        },
+        danger: '#E25858',
+      },
+    },
   },
   plugins: [],
 }
