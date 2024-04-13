ac959232a51339acd5341f7236c6234ee558f3be
reinheimermat <mreinheimer0@gmail.com>
2024-04-13 16:45:31 -0300

    style: change style

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -4,7 +4,7 @@ export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...props}
-      className="bg-grey-500 text-grey-300 p-4 rounded-lg text-lg outline-none ring-1 ring-grey-700 focus:ring-purpleDark focus:text-grey-100 w-full"
+      className="bg-grey-500 border-none text-grey-300 p-4 rounded-lg text-lg outline-none ring-1 ring-grey-700 focus:ring-purpleDark focus:text-grey-100 w-full"
     />
   )
 }
